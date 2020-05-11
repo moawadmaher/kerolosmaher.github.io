47b9eed971ec3359ec36ebb7e7a2ece3f2a6f562
moawad maher mousa <kerolosmaher@gmail.com>
2020-05-11 19:56:00 +0200

    new site

--- js/main.js
+++ js/main.js
@@ -1,18 +1,263 @@
-$(window).load(function() {
+var whmcs = false;/* For preview only */
+
+var themes = ["org","purple","darkblue","green","grey"];
+var mainColors = ["0a5dd3","756de7","142745","00b869","3d3d3d"];
+var setCssColors = "";
+var setDivs = "";
+for(var i=0;i<=themes.length-1;i++){
+    setCssColors += '.switches-holder .switch'+Number(i+1)+'{background-color: #'+mainColors[i]+';} ';
+    setDivs += '<div class="switch switch'+Number(i+1)+'" data-num="'+i+'"></div>';
+}
+
+$('body').append('<style>#theme-switcher{display: inline-block; position: fixed; top: 80px; left: 10px;z-index: 9999;} #btn-theme{padding:7px 10px;background-color:rgba(255,255,255,0.2);border-radius:7px;margin-bottom:5px;cursor:pointer;box-shadow: 0 0 10px rgba(0,0,0,0.3);}#btn-theme .fa{color:#ffffff;text-shadow: 0 0 5px rgba(0,0,0,0.7);}#btn-theme:hover{background-color:rgba(255,255,255,0.4)}.switches-holder{transform-origin:center top;transform:scaleY(0);opacity:0;padding: 7px 7px;background-color: rgba(255,255,255,0.9);border-radius: 7px;width: 34px;font-size: 0;transition: all 0.3s cubic-bezier(0.34, 1.61, 0.7, 1);box-shadow: 0 0 10px rgba(0,0,0,0.3);}.switches-holder.active{transform:scaleY(1);opacity:1;}.switches-holder .switch{display:inline-block;width:20px;height:20px;margin-bottom:5px;border-radius:4px;cursor:pointer;}.switches-holder .switch.active{box-shadow:inset 0 0 2px 2px rgba(0,0,0,0.3)} '+setCssColors+'.switches-holder div:last-child{margin-bottom:0;}</style><div id="theme-switcher"><div id="btn-theme"><i class="fa fa-paint-brush" aria-hidden="true"></i></div><div class="switches-holder">'+setDivs+'</div></div>');
+
+if(whmcs){
+    $('head').append('<link id="custom-css2" rel="stylesheet" type="text/css" href="">');
+}
+$('head').append('<link id="custom-css1" rel="stylesheet" type="text/css" href="">');
+
+
+var btnTheme=$('#btn-theme','#theme-switcher');
+
+var switchesHolder=$('.switches-holder','#theme-switcher');
+
+var themeSelected = getCookie("hostifythemeSet");
+
+for (var i=0;i<=themes.length-1;i++){
+    if(themeSelected==themes[i]){
+        if(themeSelected=="org"){
+            if(whmcs){ 
+                $('head #custom-css1').attr('href',"");
+                $('head #custom-css2').attr('href',"");
+            }else{
+                $('head #custom-css1').attr('href',"");
+            }
+        }else{
+            if(whmcs){ 
+                $('head #custom-css1').attr('href',"templates/hostify/css/style-"+themes[i]+".css");
+                $('head #custom-css2').attr('href',"templates/hostify/css/styles-modified-"+themes[i]+".css");
+            }else{
+                $('head #custom-css1').attr('href',"css/style-"+themes[i]+".css");
+            }
+        }
+        $('.switch',switchesHolder).removeClass('active');
+        $('.switch[data-num='+i+']').addClass('active');
+    }
+}
+for (var i=0;i<=themes.length-1;i++){
+    if(Number(i+1)==1){
+        $('.switches-holder .switch'+Number(i+1),'#theme-switcher').on('click',function(){
+            if(whmcs){ 
+                $('head #custom-css1').attr('href',"");
+                $('head #custom-css2').attr('href',"");
+            }else{
+                $('head #custom-css1').attr('href',"");
+            }
+            document.cookie = "hostifythemeSet="+themes[Number($(this).data("num"))];
+            setCookie("hostifythemeSet", themes[Number($(this).data("num"))], 1);
+        });
+    }else{
+        $('.switches-holder .switch'+Number(i+1),'#theme-switcher').on('click',function(){
+            if(whmcs){ 
+                $('head #custom-css1').attr('href',"templates/hostify/css/style-"+themes[Number($(this).data("num"))]+".css");
+                $('head #custom-css2').attr('href',"templates/hostify/css/styles-modified-"+themes[Number($(this).data("num"))]+".css");
+            }else{
+                $('head #custom-css1').attr('href',"css/style-"+themes[Number($(this).data("num"))]+".css");
+            }
+            document.cookie = "hostifythemeSet="+themes[Number($(this).data("num"))];
+            setCookie("hostifythemeSet", themes[Number($(this).data("num"))], 1);
+        });
+    }
+}
+
+btnTheme.on('click',function(){
+    switchesHolder.toggleClass('active');
+});
+$(".switch",switchesHolder).on('click',function(){
+    $('.switch',switchesHolder).removeClass('active');
+    $(this).addClass('active');
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+/* for preview end */
+"use strict";
+
+// Add Slider functionality to the top of home page in #top-content section.
+var mainSlider = $("#main-slider","#top-content");
+mainSlider.slick({
+    dots: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+// Adding animation to the #main-slider
+mainSlider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.slide > div:nth-child(1)','#main-slider').removeClass("animated");
+    $('.slide > div:nth-child(2)','#main-slider').removeClass("animated animation-delay1");
+ 
+    $('.slick-active > div:nth-child(1)','#main-slider').addClass("animated");
+    $('.slick-active > div:nth-child(2)','#main-slider').addClass("animated animation-delay1");
+});
+// Add Slider functionality to the #testimonials section in the home page.
+var testimonialsSlider = $("#testimonials-slider","#testimonials");
+testimonialsSlider.slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+// Add Slider functionality to the testimonials in the "Sign in" and "Sign out" pages.
+var miniTestimonialsSlider = $(".mini-testimonials-slider","#form-section");
+miniTestimonialsSlider.slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    speed: 200
+});
+// Add Slider functionality to the info-slider in the about page.
+var infoSlider = $(".info-slider","#page-head");
+infoSlider.slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    speed: 200
+});
+$(window).on("load", function() {
+    // Adding animation to the #main-slider
+    $('.slick-active > div:nth-child(1)','#main-slider').addClass("animated");
+    $('.slick-active > div:nth-child(2)','#main-slider').addClass("animated animation-delay1");
+    // Counter slider functions in "CUSTOM HOSTING PLAN" section on the homepage
+    var cPlan = $('#c-plan');
+    cPlan.slider({
+        tooltip: 'always'
+    });
+    cPlan.on("slide", function(e) {
+        $('.slider .tooltip-up','#custom-plan').text(e.value/20);
+        $('.price','#custom-plan').text($(this).data("currency") + e.value/20);
+        $('.feature1 span','#custom-plan').text(e.value);
+        $('.feature2 span','#custom-plan').text(e.value*98);
+    });
+    cPlan.value = cPlan.data("slider-value");
+    $('.slider .tooltip','#custom-plan').append('<div class="tooltip-up"></div>');
+    $('.slider .tooltip-up','#custom-plan').text(cPlan.value/20);
+    $('.slider .tooltip-inner','#custom-plan').attr("data-unit",cPlan.data("unit"));
+    $('.slider .tooltip-up','#custom-plan').attr("data-currency",cPlan.data("currency"));
+    
+    $('.price','#custom-plan').text(cPlan.data("currency") + cPlan.value/20);
+    $('.feature1 span','#custom-plan').text(cPlan.value);
+    $('.feature2 span','#custom-plan').text(cPlan.value*98);
+
+    // Features Section click function
+    var featureIconHolder = $(".feature-icon-holder", "#features-links-holder");
+    
+    featureIconHolder.on("click",function(){
+        featureIconHolder.removeClass("opened");
+        $(this).addClass("opened");
+        $(".show-details","#features-holder").removeClass("show-details");
+        $(".feature-d"+$(this).data("id"), "#features-holder").addClass("show-details");
+    });
+    
+    // Fix #features-holder height in features section
+    var featuresHolder = $("#features-holder");
+    var featuresLinksHolder = $("#features-links-holder");
+    var featureBox = $(".show-details","#features-holder");
+    
+    featuresHolder.css("height",featureBox.height()+120);
+    featuresLinksHolder.css("height",featureBox.height()+120);
+
+    // Fix #features-holder height in features section
+    $(window).on("resize",function() {
+        featuresHolder.css("height",featureBox.height()+120);
+        featuresLinksHolder.css("height",featureBox.height()+120);
+        return false;
+    });
+    
+    // Apps Section hover function
+    var appHolder = $(".app-icon-holder", "#apps");
+    
+    appHolder.on("mouseover",function(){
+        appHolder.removeClass("opened");
+        $(this).addClass("opened");
+        $(".show-details", "#apps").removeClass("show-details");
+        $(".app-details"+$(this).data("id"), "#apps").addClass("show-details");
+    });
+    
+    // More Info Section hover function
+    var infoLink = $(".info-link", "#more-info");
+    
+    infoLink.on("mouseover",function(){
+        infoLink.removeClass("opened");
+        $(this).addClass("opened");
+        $(".show-details", "#more-info").removeClass("show-details");
+        $(".info-d"+$(this).data("id"), "#more-info").addClass("show-details");
+    });
+    
+    // Servers Marker Location in our servers page
+    var locationsList = [["California",97,48,"r"],["Costa Rika",212,31,"l"],["Vancouver",136,161,"r"],["Brazil",303,233,"r"],["Alexandria",149,349,"l"],["Dubai",174,469,"l"],["Delhi",204,605,"r"],["Munech",91,417,"r"],["Barcelona",112,279,"l"],["Moscow",41,554,"r"],["Hong Kong",151,663,"r"],["Melborne",356,688,"l"],["Pulau Ujong",265,578,"l"]];
+    
+    var serversLocationHolder = $('.servers-location-holder','#serversmap.st');
+    for(var i=0;i<=locationsList.length-1;i++){
+        var sMarkerDir = locationsList[i][3];
+        var leftText = "";
+        var rightText = "";
+        if(sMarkerDir=="r"){
+            leftText = "";
+            rightText = locationsList[i][0];
+        }else if(sMarkerDir=="l"){
+            leftText = locationsList[i][0];
+            rightText = "";
+        }
+        serversLocationHolder.append('<div class="server-marker" style="top:'+locationsList[i][1]+'px;left:'+locationsList[i][2]+'px;"><span class="left-text">'+leftText+'</span><span class="marker-icon"></span><span class="right-text">'+rightText+'</span></div>');
+    }
+    
+});
+
+
+
+
+
+$(window).load(function () {
     $(".loader").fadeOut(1500);
     $("body").css("overflow-y", "auto");
 
     $(".contact-hero img.arrow").addClass("animated-arrow");
     $(".contact-page-section .info").addClass("animated-info-box");
 });
-$(document).ready(function() {
+$(document).ready(function () {
 
     /* Home Slider */
 
 
     /* branches */
 
-    $('.branches .cssr').click(function(e) {
+    $('.branches .cssr').click(function (e) {
         e.preventDefault();
         $(".branches .for-arrow").removeClass("active");
         $(this).parent().toggleClass('active');
@@ -28,8 +273,8 @@ $(document).ready(function() {
             nextArrow: $('ul.csr li.left')
         });
 
-    })
-    $('.branches .qcc').click(function(e) {
+    });
+    $('.branches .qcc').click(function (e) {
         e.preventDefault();
         $(".branches .for-arrow").removeClass("active");
         $(this).parent().toggleClass('active');
@@ -45,9 +290,9 @@ $(document).ready(function() {
             nextArrow: $('ul.qc li.left')
         });
 
-    })
+    });
 
-    $('.branches .eventss').click(function(e) {
+    $('.branches .eventss').click(function (e) {
         e.preventDefault();
         $(".branches .for-arrow").removeClass("active");
         $(this).parent().toggleClass('active');
@@ -67,7 +312,7 @@ $(document).ready(function() {
 
     $(".carousel").swipe({
 
-        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
 
             if (direction == 'left') $(this).carousel('next');
             if (direction == 'right') $(this).carousel('prev');
@@ -78,11 +323,11 @@ $(document).ready(function() {
     });
 
 
-    $(document).on('click', '.event.arrows .left', function() {
+    $(document).on('click', '.event.arrows .left', function () {
         $("section.events ul.events li.active").removeClass("active").next().addClass("active");
     });
 
-    $(document).on('click', '.event.arrows .right', function() {
+    $(document).on('click', '.event.arrows .right', function () {
         $("section.events ul.events li.active").removeClass("active").prev().addClass("active");
     });
 
@@ -99,63 +344,62 @@ $(document).ready(function() {
 
 
     // products Tabs
-    $('.products a').click(function() {
+    $('.products a').click(function () {
         $('.all-products').fadeOut();
-    })
-    $('.products a.facial').click(function() {
+    });
+    $('.products a.facial').click(function () {
         $('.products').addClass('facial').removeClass('Kitchen pocket Toilet Napkins');
-    })
-    $('.products a.Kitchen').click(function() {
+    });
+    $('.products a.Kitchen').click(function () {
         $('.products').addClass('Kitchen').removeClass('facial pocket Toilet Napkins');
-    })
-    $('.products a.pocket').click(function() {
+    });
+    $('.products a.pocket').click(function () {
         $('.products').addClass('pocket').removeClass('facial Kitchen Toilet Napkins');
-    })
-    $('.products a.Toilet').click(function() {
+    });
+    $('.products a.Toilet').click(function () {
         $('.products').addClass('Toilet').removeClass('facial Kitchen pocket Napkins');
-    })
-    $('.products a.Napkins').click(function() {
+    });
+    $('.products a.Napkins').click(function () {
         $('.products').addClass('Napkins').removeClass('facial Kitchen pocket Toilet');
-    })
-
+    });
     // change img color
 
-    $('.products a.facial').mouseenter(function() {
+    $('.products a.facial').mouseenter(function () {
         $(this).find('img').attr("src", "img/products/product1-wh.png");
-    })
-    $('.products a.facial').mouseleave(function() {
+    });
+    $('.products a.facial').mouseleave(function () {
         $(this).find('img').attr("src", "img/products/product1.png");
-    })
-    $('.products a.kitchen').mouseenter(function() {
+    });
+    $('.products a.kitchen').mouseenter(function () {
         $(this).find('img').attr("src", "img/products/product2-wh.png");
-    })
-    $('.products a.kitchen').mouseleave(function() {
+    });
+    $('.products a.kitchen').mouseleave(function () {
         $(this).find('img').attr("src", "img/products/product2.png");
-    })
+    });
 
-    $('.products a.pocket').mouseenter(function() {
+    $('.products a.pocket').mouseenter(function () {
         $(this).find('img').attr("src", "img/products/product3-wh.png");
-    })
-    $('.products a.pocket').mouseleave(function() {
+    });
+    $('.products a.pocket').mouseleave(function () {
         $(this).find('img').attr("src", "img/products/product3.png");
-    })
+    });
 
-    $('.products a.Toilet').mouseenter(function() {
+    $('.products a.Toilet').mouseenter(function () {
         $(this).find('img').attr("src", "img/products/product4-wh.png");
-    })
-    $('.products a.Toilet').mouseleave(function() {
+    });
+    $('.products a.Toilet').mouseleave(function () {
         $(this).find('img').attr("src", "img/products/product4.png");
-    })
-    $('.products a.Napkins').mouseenter(function() {
+    });
+    $('.products a.Napkins').mouseenter(function () {
         $(this).find('img').attr("src", "img/products/product5-wh.png");
-    })
-    $('.products a.Napkins').mouseleave(function() {
+    });
+    $('.products a.Napkins').mouseleave(function () {
         $(this).find('img').attr("src", "img/products/product5.png");
-    })
+    });
 
 
     /* Add new dropdown */
-    $(".add-more").click(function() {
+    $(".add-more").click(function () {
         $(".education .col-md-7:last").after('<div class="col-md-7 col-xs-12"><div class="form-group"><label>Educational Level</label><select class="form-control"><option>Choose ...</option><option>1</option><option>2</option><option>3</option><option>4</option></select></div></div>');
     });
 
@@ -165,7 +409,7 @@ $(document).ready(function() {
     });
 });
 
-$(window).load(function() {
+$(window).load(function () {
     // products
 
     $('.nylon-slider').slick({
@@ -399,14 +643,14 @@ $(window).load(function() {
 
 
 
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         // init Isotope
         var $grid = $('.items').isotope({
             // options
 
         });
         // filter items on button click
-        $('.filter-button-group').on('click', 'button', function() {
+        $('.filter-button-group').on('click', 'button', function () {
             $(this).addClass('active').siblings().removeClass('active');
             $(this).addClass('in').siblings().removeClass('in');
             var filterValue = $(this).attr('data-filter');
@@ -416,13 +660,13 @@ $(window).load(function() {
             } else {
                 $('.items').removeClass('visible');
             }
-        })
-        $('.filter-group').on('click', 'button', function() {
+        });
+        $('.filter-group').on('click', 'button', function () {
             $(this).addClass('active').siblings().removeClass('active');
             $(this).addClass('in').siblings().removeClass('in');
             var filterValue = $(this).attr('data-filter');
             $grid.isotope({ filter: filterValue });
-        })
+        });
     });
 
 
